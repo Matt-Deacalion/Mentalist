6835821e5e970962b61845282a842f946901b6dd
Matt Deacalion Stevens <matt@dirtymonkey.co.uk>
2014-12-05 13:43:58 +0000

    Add core app

---
+++ mentalist/apps/core/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from .views import HomeView
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+)
