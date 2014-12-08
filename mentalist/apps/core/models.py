0690e7e9e63c44d8e7cd26d25fecc6bf06470fb5
Matt Deacalion Stevens <matt@dirtymonkey.co.uk>
2014-12-08 14:25:32 +0000

    Add stubs and docstrings for the core models.

--- mentalist/apps/core/models.py
+++ mentalist/apps/core/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class LittlePrinter(models.Model):
+    """
+    A physical Little Printer.
+    """
+    pass
+
+
+class Pearl(models.Model):
+    """
+    A piece of information which when acted on over several iterations will
+    result in the user memorising something. The piece of information itself
+    does not have to be the thing being memorised, it could be a trigger or
+    anything that causes concious effort to be applied to the information.
+
+    This entity could be any of the following:
+
+        + Question
+        + Task
+        + Fact
+        + Definition
+        + Image
+    """
+    pass
+
+
+class Iteration(models.Model):
+    """
+    An iteration of an instance of a `Pearl`. There are six of these for every
+    instance.
+    """
+    pass
+
+
+class Delivery(models.Model):
+    """
+    An actual delivery that is sent to a specific Little Printer.
+    """
+    pass
