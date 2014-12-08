eea3da34c3e8a7c7edeca56aaf351be7266aaf60
Matt Deacalion Stevens <matt@dirtymonkey.co.uk>
2014-12-08 15:48:33 +0000

    Add the new core models to the Django admin

--- mentalist/apps/core/admin.py
+++ mentalist/apps/core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import LittlePrinter, Pearl, Iteration
+
+
+class LittlePrinterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+admin.site.register(LittlePrinter, LittlePrinterAdmin)
+
+
+class PearlAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status', 'text', 'answer', 'image', 'minutes')
+admin.site.register(Pearl, PearlAdmin)
+
+
+class IterationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'pearl', 'date', 'iteration')
+    list_filter = ('user', 'pearl', 'date')
+admin.site.register(Iteration, IterationAdmin)
