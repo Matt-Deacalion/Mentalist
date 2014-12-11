65d5ba36bea1eb175bedad923bd044e003410900
Matt Deacalion Stevens <matt@dirtymonkey.co.uk>
2014-12-11 13:11:20 +0000

    Implement views and add unit tests for context data

--- mentalist/apps/core/tests.py
+++ mentalist/apps/core/tests.py
@@ -1,5 +1,12 @@
-from django.core.urlresolvers import resolve
-from django.test import TestCase
+from datetime import date, timedelta
+
+from django.core.urlresolvers import resolve, reverse
+from django.http import Http404
+from django.test import RequestFactory, TestCase
+from model_mommy import mommy
+
+from .models import Iteration
+from .views import DayEditionView, TodayEditionView
 
 
 class EditionURLTests(TestCase):
@@ -26,3 +33,162 @@ class EditionURLTests(TestCase):
         response = self.client.get('/edition/matt/2014/dec/25/')
         view = resolve(response.request['PATH_INFO'])[0]
         self.assertEqual(view.__name__, 'DayEditionView')
+
+
+class EditionContextTests(TestCase):
+    """
+    Do the views contain the correct context data?
+    """
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+        self.becia = mommy.make('auth.User')
+        self.matt = mommy.make('auth.User')
+        self.ewa = mommy.make('auth.User')
+
+    def test_user_does_not_exist(self):
+        """
+        If the user does not exist, return a 404.
+        """
+        with self.assertRaises(Http404):
+            username = 'JonSnow'
+
+            url = reverse('edition_today', args=[username])
+            request = self.factory.get(url)
+            TodayEditionView.as_view()(request, user=username)
+
+    def test_user_empty(self):
+        """
+        Iterations exist for other users, but not for `Ewa`. Return a 404 if
+        the user has no iterations.
+        """
+        mommy.make('core.Iteration', date=date.today(), user=self.becia)
+        mommy.make('core.Iteration', date=date.today(), user=self.matt)
+
+        with self.assertRaises(Http404):
+            username = self.ewa.username
+
+            url = reverse('edition_today', args=[username])
+            request = self.factory.get(url)
+            TodayEditionView.as_view()(request, user=username)
+
+    def test_user_limited_today(self):
+        """
+        Ability to limit iterations to a user and those which are active today.
+        """
+        user = self.matt
+        username = self.matt.username
+
+        # Iterations for today. These `should` show.
+        iterations = [
+            mommy.make('core.Iteration', date=date.today(), user=user),
+            mommy.make('core.Iteration', date=date.today(), user=user),
+            mommy.make('core.Iteration', date=date.today(), user=user),
+        ]
+
+        # Iterations in the past and future. These should `not` show.
+        mommy.make('core.Iteration', date=date.today()-timedelta(5), user=user)
+        mommy.make('core.Iteration', date=date.today()+timedelta(5), user=user)
+
+        url = reverse('edition_today', args=[username])
+        request = self.factory.get(url)
+        response = TodayEditionView.as_view()(request, user=username)
+
+        self.assertEqual(
+            list(response.context_data['iteration_list']),
+            list(iterations),
+        )
+
+    def test_future_date(self):
+        """
+        Ability to limit iterations to a specified user and a date in the
+        future.
+        """
+        user = self.matt
+        username = self.matt.username
+        future_date = date.today() + timedelta(5)
+
+        # Iteration on a future date. This `should` show.
+        mommy.make('core.Iteration', date=future_date, user=user)
+
+        # Iterations for today and in the past. These should `not` show.
+        mommy.make('core.Iteration', date=date.today(), user=user)
+        mommy.make('core.Iteration', date=date.today()-timedelta(5), user=user)
+
+        url = reverse('edition_day', args=[
+            username,
+            future_date.year,
+            future_date.month,
+            future_date.day,
+        ])
+
+        request = self.factory.get(url)
+
+        response = DayEditionView.as_view(month_format='%m')(
+            request,
+            user=username,
+            year=str(future_date.year),
+            month=str(future_date.month),
+            day=str(future_date.day),
+        )
+
+        self.assertEqual(
+            list(response.context_data['iteration_list']),
+            list(Iteration.objects.filter(date=future_date)),
+        )
+
+    def test_past_date(self):
+        """
+        Ability to limit iterations to a user and a date in the past.
+        """
+        user = self.matt
+        username = self.matt.username
+        past_date = date.today() - timedelta(5)
+
+        # Iteration on a past date. This `should` show.
+        mommy.make('core.Iteration', date=past_date, user=user)
+
+        # Iterations for today and in the future. These should `not` show.
+        mommy.make('core.Iteration', date=date.today(), user=user)
+        mommy.make('core.Iteration', date=date.today()+timedelta(5), user=user)
+
+        url = reverse('edition_day', args=[
+            username,
+            past_date.year,
+            past_date.month,
+            past_date.day,
+        ])
+
+        request = self.factory.get(url)
+
+        response = DayEditionView.as_view(month_format='%m')(
+            request,
+            user=username,
+            year=str(past_date.year),
+            month=str(past_date.month),
+            day=str(past_date.day),
+        )
+
+        self.assertEqual(
+            list(response.context_data['iteration_list']),
+            list(Iteration.objects.filter(date=past_date)),
+        )
+
+    def test_total_time(self):
+        """
+        Does the template context have the correct total time for iterations?
+        """
+        user = self.matt
+        username = self.matt.username
+
+        pearl = mommy.make('core.Pearl', minutes=3)
+        mommy.make('core.Iteration', date=date.today(), user=user, pearl=pearl)
+        mommy.make('core.Iteration', date=date.today(), user=user, pearl=pearl)
+        mommy.make('core.Iteration', date=date.today(), user=user, pearl=pearl)
+
+        url = reverse('edition_today', args=[username])
+        request = self.factory.get(url)
+        response = TodayEditionView.as_view()(request, user=username)
+
+        self.assertEqual(response.context_data['total_time'], '9 minutes')
--- mentalist/apps/core/views.py
+++ mentalist/apps/core/views.py
@@ -8,15 +8,46 @@ class EditionView(object):
     The main printout for the Little Printer. In their documentation they call
     this an `edition`. This lists all the `iterations` for a particular user.
     """
+    context_object_name = 'iteration_list'
+    template_name = 'base.html'
     date_field = 'date'
     model = Iteration
+
+    def get_queryset(self):
+        """
+        Retrieve only the `iterations` that belong to a specific user.
+        """
+        user = self.kwargs.get('user')
+        return super().get_queryset().filter(user__username=user)
+
+    def get_context_data(self, **kwargs):
+        """
+        This has been overridden to give the template access to the amount of
+        time it would take to complete all iterations.
+        """
+        context = super().get_context_data(**kwargs)
+        context['total_time'] = self.get_total_time(kwargs['object_list'])
+        return context
+
+    def get_total_time(self, iterations):
+        """
+        Takes a list of iteration instances and returns a string that tells
+        you how many minutes it will take to complete them all.
+        """
+        minutes = sum([i.pearl.minutes for i in iterations])
+        total_time = '{} minute'.format(minutes)
+
+        if minutes > 1:
+            total_time += 's'
+
+        return total_time
 
 
 class DayEditionView(EditionView, DayArchiveView):
     """
     Limit the list of returned `iterations` to a specified day.
     """
-    pass
+    allow_future = True
 
 
 class TodayEditionView(EditionView, TodayArchiveView):
